2780602bcf699353f9a973fb025c4c4945390549
htikelinn <htikezawlinn@gmail.com>
2025-09-26 10:58:57 +0630

    separt Item and List are each file

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,7 @@
 import React from 'react'
+import List from './List'
+import Item from './Item'
 
-function Item({ content, name }) {
-  return (
-    <li style={{ padding: 10, borderBottom: "1px solid #ddd" }}>
-      {content} - <b>{name}</b>
-    </li>
-  )
-}
-
-function List({ children }) {
-  return (
-    <ul style={{
-      listStyle: "none",
-      padding: 0,
-      margin: 0,
-      border: "1px solid #ddd",
-      borderRadius: 8,
-      overflow: "hidden",
-    }}>
-      {children}
-    </ul>
-  )
-}
 export default function App() {
   return (
     <div style={{ maxWidth: 600, margin: "20px auto"}}>
