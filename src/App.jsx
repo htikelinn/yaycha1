d4a4a754ab3f7f137d9feded938345eccd68eea8
htikelinn <htikezawlinn@gmail.com>
2025-09-26 11:07:15 +0630

    add useState Component in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import List from './List'
 import Item from './Item'
 
 export default function App() {
+  const [data, setData] = useState([
+    { id: 1, content : "Hello Htin",     name : "Htin" },
+    { id: 2, content : "React tutorial", name : "Phyo" },
+    { id: 3, content : "I love Phyo",    name : "Zaw" }])
   return (
-    <div style={{ maxWidth: 600, margin: "20px auto"}}>
+
+    <div style={{ maxWidth: 600, margin: "20px auto" }}>
       <h1>What on you mind?</h1>
       <List>
-        <Item content="Hello Htin" name="Htin" />
-        <Item content="React tutorial" name="Phyo" />
-        <Item content="I love Phyo" name="Zaw" />
+        {data.map(i => {
+          return <Item key={i.id} content={i.content} name={i.name} />
+        })}
       </List>
     </div>
   )
