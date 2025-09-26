2ad8cc41d6db6998a6ca97b1dda57ef7c37c2943
htikelinn <htikezawlinn@gmail.com>
2025-09-26 11:18:15 +0630

    add remove button in Item and updae App

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,17 @@ export default function App() {
     { id: 1, content : "Hello Htin",     name : "Htin" },
     { id: 2, content : "React tutorial", name : "Phyo" },
     { id: 3, content : "I love Phyo",    name : "Zaw" }])
+
+    const remove = id => {
+      setData(data.filter(item => item.id !== id))
+    }
   return (
 
     <div style={{ maxWidth: 600, margin: "20px auto" }}>
       <h1>What on you mind?</h1>
       <List>
         {data.map(i => {
-          return <Item key={i.id} content={i.content} name={i.name} />
+          return <Item key={i.id} item={i} remove={remove}/>
         })}
       </List>
     </div>
--- src/Item.jsx
+++ src/Item.jsx
@@ -1,8 +1,12 @@
 
-export default function Item({ content, name }) {
+export default function Item({ item, remove }) {
   return (
     <li style={{ padding: 10, borderBottom: "1px solid #ddd" }}>
-      {content} - <b>{name}</b>
+        <span>
+
+      {item.content} - <b>{item.name}</b>
+        </span>
+        <button onClick={() => remove(item.id)}>DELETE</button>
     </li>
   )
 }
